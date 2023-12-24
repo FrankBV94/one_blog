a67148463874d20fedcd0fc5dee405ee0cdd08ab
FrankBV94 <frankbotelle@gmail.com>
2023-12-23 22:59:57 -0500

    New TS rules

--- one_blog_web/.eslintrc.cjs
+++ one_blog_web/.eslintrc.cjs
@@ -34,6 +34,7 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/no-misused-promises': 'off',
         '@typescript-eslint/no-confusing-void-expression': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off'
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off'
     }
 }
