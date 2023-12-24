40421aca23a02e83f8235d133c93626d9f38a007
FrankBV94 <frankbotelle@gmail.com>
2023-12-23 23:10:24 -0500

    Query Provider

--- one_blog_web/src/main.tsx
+++ one_blog_web/src/main.tsx
@@ -3,21 +3,16 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import {
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query'
+import { QueryProvider } from './queries/queryProvider'
 import { AuthProvider } from './context/AuthContext'
-
-const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
+    <QueryProvider >
       <AuthProvider>
         <App />
       </AuthProvider>
-    </QueryClientProvider>
+    </QueryProvider>
     <Toaster
       position="top-center"
       reverseOrder={false}
