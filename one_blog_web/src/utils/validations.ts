0d947a378f84e436447402f69aa626e8916c2271
FrankBV94 <frankbotelle@gmail.com>
2023-12-23 23:06:38 -0500

    Post form validation

--- one_blog_web/src/utils/validations.ts
+++ one_blog_web/src/utils/validations.ts
@@ -1,8 +1,6 @@
 import * as z from 'zod'
 
-// ============================================================
-// USER
-// ============================================================
+/** SIGN UP VALIDATION */
 export const SignupValidation = z.object({
   name: z.string().min(2, { message: 'El nombre debe tener al menos 2 caracteres.' }),
   username: z.string().min(2, { message: 'El usuario debe tener al menos 2 caracteres.' }),
@@ -10,7 +8,19 @@ export const SignupValidation = z.object({
   password: z.string().min(8, { message: 'La contraseña debe tener al menos 8 caracteres.' })
 })
 
+/** SIGN IN VALIDATION */
 export const SigninValidation = z.object({
   email: z.string().email(),
   password: z.string()
+})
+
+/** CREATE POST VALIDATION */
+export const PostValidation = z.object({
+  tittle: z.string().min(5, { message: 'Minimum 5 characters.' }).max(100, { message: 'Maximum 100 caracters' }),
+  banner: z.custom<File[]>(),
+  des: z.string().min(5, { message: 'Minimum 5 characters.' }).max(2200, { message: 'Maximum 200 caracters' }),
+  content: z.custom<[]>(),
+  tags: z.string(),
+  draf: z.boolean(),
+  author: z.string()
 })
